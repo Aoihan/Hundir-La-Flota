77d01a56c9ad8ac86c95cb88bb52c1ff01b5f67b
afd <afd@alup5.academiapostal.es.local>
2020-02-10 20:00:43 +0100

    terminar de poner cookies.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var jugadores = 0;
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Juego de Hundir la flota' });
+  
+  const session = req.session;
+  
+  if (!session.jugador) {
+    jugadores++;
+    session.jugador = jugadores;
+  }
+  res.render('index', { title: 'Juego de Hundir la flota',jugadores });
 });
 
 module.exports = router;
+
